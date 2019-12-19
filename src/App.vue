148cdb8a21509948d435dbcedf64c016d6bb170b
oussamaAlouat <al317257@uji.es>
2019-12-19 11:29:42 +0100

    Extract login code from app component

--- src/App.vue
+++ src/App.vue
@@ -3,37 +3,11 @@
     <v-app-bar app color="primary" dark>
       <v-toolbar-title>Vuetify Dahboard</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn text rounded>Home</v-btn>
-      <v-btn text rounded>Login</v-btn>
+      <v-btn text rounded to="/">Home</v-btn>
+      <v-btn text rounded to="/login">Login</v-btn>
     </v-app-bar>
     <v-content>
-      <!-- Login Module -->
-      <v-card width="400" class="mx-auto mt-5">
-        <v-card-title>
-          <h1 class="display-1">Login</h1>
-        </v-card-title>
-        <v-card-text>
-          <v-form>
-            <v-text-field
-              label="Username"
-              prepend-icon="mdi-account-circle"
-            />
-            <v-text-field
-              label="Password"
-              :type="showPassword ?'text': 'password'"
-              prepend-icon="mdi-lock"
-              :append-icon="showPassword ? 'mdi-eye': 'mdi-eye-off'"
-              @click:append="showPassword = !showPassword"
-            />
-          </v-form>
-        </v-card-text>
-        <v-divider></v-divider>
-        <v-card-actions>
-          <v-btn color="success">Register</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn color="info">Login</v-btn>
-        </v-card-actions>
-      </v-card>
+      <router-view></router-view>
     </v-content>
     <v-footer
       color="primary lighten-1"
@@ -73,7 +47,6 @@ export default {
   },
 
   data: () => ({
-    showPassword: false,
     links: [
       'Home',
       'Login',
